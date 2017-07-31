3f79e74a78fb8947416e4ddc0ac024bdf4b721f9
Henry Li <henry1943@163.com>
2017-07-31 20:54:27 +0800

    Add eslint-loader

--- webpack/development.js
+++ webpack/development.js
@@ -1,3 +1,20 @@
+const webpack = require('webpack');
+
 const config = require('./base');
 
 module.exports = config;
+config.module.rules.push(
+  {
+    enforce: 'pre',
+    test: /\.jsx?$/,
+    use: ['eslint-loader'],
+    exclude: /node_modules/
+  }
+);
+
+config.plugins.push(
+  new webpack.SourceMapDevToolPlugin({
+    filename: '[file].map',
+    exclude: ['vendor.js']
+  })
+);
