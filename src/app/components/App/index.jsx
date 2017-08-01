1afa33e6911b0b393ea6af5ab05dd13287231b5f
Henry Li <henry1943@163.com>
2017-08-01 20:24:19 +0800

    Connect to redux , and add index.less

--- src/app/components/App/index.jsx
+++ src/app/components/App/index.jsx
@@ -1,10 +1,37 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 
+import actions from '../../actions';
+
+import styles from './index.less';
+
+@connect(
+  state => ({ ...state }),
+  dispatch => ({ dispatch, actions: bindActionCreators(actions, dispatch) })
+)
 export default class App extends PureComponent {
+  static propTypes = {
+    message: PropTypes.string,
+    actions: PropTypes.shape({
+      hello: PropTypes.func
+    }).isRequired
+  }
+
+  static defaultProps = {
+    message: null
+  }
+
+  componentDidMount() {
+    this.props.actions.hello('from server-side');
+  }
+
   render() {
+    const { message } = this.props;
     return (
-      <div>
-        <h1>Hello, world</h1>
+      <div className={styles.container}>
+        <h1>{message}</h1>
       </div>
     );
   }
