ff106c5f9d1b5300e04ed672d33cd493d87c1762
Henry Li <henry1943@163.com>
2017-08-01 20:22:40 +0800

    Connect to redux

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,16 +1,23 @@
 import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDom from 'react-dom';
 
+
+import { configStore } from './store';
 import App from './components/App';
 
 import './index.less';
+
+const store = configStore();
 
 function renderMountPoint() {
   ReactDom.render(
     (
       <AppContainer>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </AppContainer>
     ),
     document.getElementById('mount-point')
